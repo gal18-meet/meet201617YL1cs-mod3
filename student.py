cde9b18bcbb941632bd002ced196998fd519331f
gal18-meet <levtov.gal@gmail>
2016-12-08 19:59:57 +0200

    test

---
+++ student.py
@@ -0,0 +1,17 @@
+class Student :
+
+    def __init__ (self, hometown, age, height, icecream):
+        self.hometown=hometown
+        self.age=age
+        self.height=height
+        self.icecream=icecream
+        
+        
+
+
+
+
+    
+
+    def print_summary(self):
+        print(self.hometown, self.age, self.height, self.icecream)
